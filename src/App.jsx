1ce5e319ff194ef60ef27c0e778c9b7d8b56d4a5
Sahil Khan <sahil4t9@gmail.com>
2026-05-23 20:42:29 +0530

    Add the components in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <Header />
+      <Note />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
