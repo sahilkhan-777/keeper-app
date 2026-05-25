7cc1a8ea76f18654fbc38b36b1cffb4a0d7f7739
Sahil Khan <sahil4t9@gmail.com>
2026-05-25 21:36:48 +0530

    Import the notes object into the App and pass them over as props to Note component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import "./App.css";
+import notes from "./notes";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
       <Footer />
     </>
   );
