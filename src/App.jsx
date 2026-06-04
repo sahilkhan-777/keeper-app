32782b61a97639522908b7e380110f52eaa76de6
Sahil Khan <sahil4t9@gmail.com>
2026-06-04 21:23:23 +0530

    Add a note state and functions to add and delete notes which are then passed onto Note and CreateArea components as props

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,43 @@ import Footer from "./components/Footer";
 import Note from "./components/Note";
 import "./App.css";
 import notes from "./notes";
+import { useState } from "react";
+import CreateArea from "./components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <>
+    <div>
       <Header />
-      {notes.map((note) => {
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
         return (
-          <Note key={note.key} title={note.title} content={note.content} />
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
         );
       })}
       <Footer />
-    </>
+    </div>
   );
 }
 
