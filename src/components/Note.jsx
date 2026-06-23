699675e79a19cff015066e896c013e631a306a5d
Sahil Khan <sahil4t9@gmail.com>
2026-06-23 21:30:36 +0530

    feat: Replace  delete button text with DeleteIcon from the material icons library

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,18 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+
 function Note(props) {
   function handleClick() {
     props.onDelete(props.id);
   }
 
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
-    </div>
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <button onClick={handleClick}>
+          <DeleteIcon />
+        </button>
+      </div>
   );
 }
 
